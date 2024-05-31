e5fd4bad4a15f71e33b8badd1b92b7e080904986
gab-hub <gng2112@columbia.edu>
2024-05-31 15:56:28 -0700

    trying to start testing with catch2

--- src/lru_cache.hpp
+++ src/lru_cache.hpp
@@ -1,3 +1,8 @@
+/* Gabriela Gonzalez
+April 18, 2024
+This program creates an LRU cache */
+
+
 #ifndef LRU_CACHE_HPP
 #define LRU_CACHE_HPP
 
@@ -7,42 +12,6 @@
 #include <unordered_set>
 #include <vector>
 
-// call print in put and then make print private
-// preprocess definition ndebug --> check debug
-//reorganize this file
-/* 
-#ifdef NDEBUG
-	#define log_info(...)
-	#define display_lru()
-#else
-	#define log_info(...) printf(__VA_ARGS__);
-	#define display_lru() display();
-#endif
-
-^ have different functions here if you are in debug or no
-*/
-// search for it in the cache rotate and push to the top of the stack
-// if it found the item (and print it there)
-// testing suite
-// contracts: See if size input is int ,Check if input later is URL for specific kind of cache 
-// Generic container lru cache and outer cache makes it more specific URL 
-// Use assert (see alan 1:1 links) and catch3 libraries 
-// See if you can use this 
-// https://git.alteryx.com/devsec/cryptolib/-/blob/latest-nightly/Library/include/CryptoImpl/Contracts.h 
-// https://git.alteryx.com/devsec/katas/-/tree/katas_cpp_alan?ref_type=heads 
-/*volatile
-§ Lambda function for rotate’  
-§ destructor
-§ Give back a const copy bc it’s a library  --> actually no 
-□ Think how vectors work and write library similar 
-□ New vector, rvo to copy, and return? 
-◊ Return value optimization 
-□ For comments, what would it look like to implement? Why would i do this? 
-□ Do const iterator... can’t change underlying data structure bc it’s just a pointer under the hood 
-□ Look at vector implementation 
-□ Implent index ones and at 
-Access to use range-based algorithm in case people decide to filter*/
-// gcc file treat warnings as error -wall?
 
 template <typename T>
 class lru_cache
@@ -60,11 +29,6 @@ private:
     bool correct_cache() const;
     void rearrange_cache(const T& url);
 };
-
-/* Gabriela Gonzalez
-April 18, 2024
-This program creates an LRU cache */
-
 
 
 /* Places item in cache and checks for other important conditions */
